1a8d18d439534386325d5d9fae4b091673477cd2
Edi Modrić <edi.modric@gmail.com>
2016-03-21 16:56:36 +0100

    Add tests for backend

---
+++ Tests/Backend/EzPublishBackendTest.php
@@ -0,0 +1,339 @@
+<?php
+
+namespace Netgen\Bundle\ContentBrowserBundle\Tests\Backend;
+
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchResult;
+use eZ\Publish\Core\Repository\Values\Content\Location;
+use eZ\Publish\API\Repository\Values\Content\Location as APILocation;
+use Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend;
+
+class EzPublishBackendTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \eZ\Publish\API\Repository\SearchService|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $searchServiceMock;
+
+    /**
+     * @var array
+     */
+    protected $config = array();
+
+    /**
+     * @var \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend
+     */
+    protected $backend;
+
+    public function setUp()
+    {
+        $this->searchServiceMock = self::getMock(SearchService::class);
+
+        $this->config = array(
+            'root_items' => array(1, 43, 5),
+            'default_limit' => 25
+        );
+
+        $this->backend = new EzPublishBackend(
+            $this->searchServiceMock,
+            $this->config
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::__construct
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::getSections
+     */
+    public function testGetSections()
+    {
+        foreach ($this->config['root_items'] as $index => $rootItemId) {
+            $query = new LocationQuery();
+            $query->filter = new Criterion\LocationId($rootItemId);
+
+            $searchResult = new SearchResult();
+            $searchResult->searchHits = array(
+                new SearchHit(array('valueObject' => new Location()))
+            );
+
+            $this->searchServiceMock
+                ->expects($this->at($index))
+                ->method('findLocations')
+                ->with($this->equalTo($query))
+                ->will($this->returnValue($searchResult));
+        }
+
+        $sections = $this->backend->getSections();
+
+        self::assertCount(3, $sections);
+        foreach ($sections as $section) {
+            self::assertInstanceOf(APILocation::class, $section);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::loadItem
+     */
+    public function testLoadItem()
+    {
+        $query = new LocationQuery();
+        $query->filter = new Criterion\LocationId(1);
+
+        $searchResult = new SearchResult();
+        $searchResult->searchHits = array(
+            new SearchHit(array('valueObject' => new Location()))
+        );
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $item = $this->backend->loadItem(1);
+
+        self::assertInstanceOf(APILocation::class, $item);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::loadItem
+     * @expectedException \Netgen\Bundle\ContentBrowserBundle\Exceptions\NotFoundException
+     */
+    public function testLoadItemThrowsNotFoundException()
+    {
+        $query = new LocationQuery();
+        $query->filter = new Criterion\LocationId(1);
+
+        $searchResult = new SearchResult();
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $this->backend->loadItem(1);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::getChildren
+     */
+    public function testGetChildren()
+    {
+        $query = new LocationQuery();
+        $query->offset = 0;
+        $query->limit = $this->config['default_limit'];
+        $query->filter = new Criterion\LogicalAnd(
+            array(
+                new Criterion\ParentLocationId(1)
+            )
+        );
+
+        $searchResult = new SearchResult();
+        $searchResult->searchHits = array(
+            new SearchHit(array('valueObject' => new Location())),
+            new SearchHit(array('valueObject' => new Location()))
+        );
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $items = $this->backend->getChildren(1);
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(APILocation::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::getChildren
+     */
+    public function testGetChildrenWithParams()
+    {
+        $query = new LocationQuery();
+        $query->offset = 5;
+        $query->limit = 10;
+        $query->filter = new Criterion\LogicalAnd(
+            array(
+                new Criterion\ParentLocationId(1),
+                new Criterion\ContentTypeIdentifier(array('type1', 'type2'))
+            )
+        );
+
+        $searchResult = new SearchResult();
+        $searchResult->searchHits = array(
+            new SearchHit(array('valueObject' => new Location())),
+            new SearchHit(array('valueObject' => new Location()))
+        );
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $items = $this->backend->getChildren(
+            1,
+            array(
+                'offset' => 5,
+                'limit' => 10,
+                'types' => array('type1', 'type2')
+            )
+        );
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(APILocation::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::getChildrenCount
+     */
+    public function testGetChildrenCount()
+    {
+        $query = new LocationQuery();
+        $query->limit = 0;
+        $query->filter = new Criterion\LogicalAnd(
+            array(
+                new Criterion\ParentLocationId(1)
+            )
+        );
+
+        $searchResult = new SearchResult();
+        $searchResult->totalCount = 2;
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $count = $this->backend->getChildrenCount(1);
+
+        self::assertEquals(2, $count);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::getChildrenCount
+     */
+    public function testGetChildrenCountWithParams()
+    {
+        $query = new LocationQuery();
+        $query->limit = 0;
+        $query->filter = new Criterion\LogicalAnd(
+            array(
+                new Criterion\ParentLocationId(1),
+                new Criterion\ContentTypeIdentifier(array('type1', 'type2'))
+            )
+        );
+
+        $searchResult = new SearchResult();
+        $searchResult->totalCount = 2;
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $count = $this->backend->getChildrenCount(
+            1,
+            array('types' => array('type1', 'type2'))
+        );
+
+        self::assertEquals(2, $count);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::search
+     */
+    public function testSearch()
+    {
+        $query = new LocationQuery();
+        $query->offset = 0;
+        $query->limit = $this->config['default_limit'];
+        $query->filter = new Criterion\FullText('test');
+
+        $searchResult = new SearchResult();
+        $searchResult->searchHits = array(
+            new SearchHit(array('valueObject' => new Location())),
+            new SearchHit(array('valueObject' => new Location()))
+        );
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $items = $this->backend->search('test');
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(APILocation::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::search
+     */
+    public function testSearchWithParams()
+    {
+        $query = new LocationQuery();
+        $query->offset = 5;
+        $query->limit = 10;
+        $query->filter = new Criterion\FullText('test');
+
+        $searchResult = new SearchResult();
+        $searchResult->searchHits = array(
+            new SearchHit(array('valueObject' => new Location())),
+            new SearchHit(array('valueObject' => new Location()))
+        );
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $items = $this->backend->search(
+            'test',
+            array('offset' => 5, 'limit' => 10)
+        );
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(APILocation::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzPublishBackend::searchCount
+     */
+    public function testSearchCount()
+    {
+        $query = new LocationQuery();
+        $query->limit = 0;
+        $query->filter = new Criterion\FullText('test');
+
+        $searchResult = new SearchResult();
+        $searchResult->totalCount = 2;
+
+        $this->searchServiceMock
+            ->expects($this->once())
+            ->method('findLocations')
+            ->with($this->equalTo($query))
+            ->will($this->returnValue($searchResult));
+
+        $count = $this->backend->searchCount('test');
+
+        self::assertEquals(2, $count);
+    }
+}
---
+++ Tests/Backend/EzTagsBackendTest.php
@@ -0,0 +1,317 @@
+<?php
+
+namespace Netgen\Bundle\ContentBrowserBundle\Tests\Backend;
+
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
+use Netgen\TagsBundle\API\Repository\TagsService;
+use Netgen\TagsBundle\API\Repository\Values\Tags\Tag;
+use Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend;
+
+class EzTagsBackendTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Netgen\TagsBundle\API\Repository\TagsService|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $tagsServiceMock;
+
+    /**
+     * @var array
+     */
+    protected $config = array();
+
+    /**
+     * @var \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend
+     */
+    protected $backend;
+
+    public function setUp()
+    {
+        $this->tagsServiceMock = self::getMock(TagsService::class);
+
+        $this->config = array(
+            'root_items' => array(4, 2),
+            'default_limit' => 25
+        );
+
+        $this->backend = new EzTagsBackend(
+            $this->tagsServiceMock,
+            $this->config
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::__construct
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::getSections
+     */
+    public function testGetSections()
+    {
+        foreach ($this->config['root_items'] as $index => $rootItemId) {
+            $this->tagsServiceMock
+                ->expects($this->at($index))
+                ->method('loadTag')
+                ->with($this->equalTo($rootItemId))
+                ->will($this->returnValue(new Tag()));
+        }
+
+        $sections = $this->backend->getSections();
+
+        self::assertCount(2, $sections);
+        foreach ($sections as $section) {
+            self::assertInstanceOf(Tag::class, $section);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::loadItem
+     */
+    public function testLoadItem()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTag')
+            ->with($this->equalTo(1))
+            ->will($this->returnValue(new Tag()));
+
+        $tag = $this->backend->loadItem(1);
+
+        self::assertInstanceOf(Tag::class, $tag);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::loadItem
+     */
+    public function testLoadItemWithEmptyItemId()
+    {
+        $this->tagsServiceMock
+            ->expects($this->never())
+            ->method('loadTag');
+
+        $tag = $this->backend->loadItem(0);
+
+        self::assertEquals(
+            new Tag(
+                array(
+                    'id' => 0,
+                    'keywords' => array(
+                        'eng-GB' => 'All tags',
+                    ),
+                    'mainLanguageCode' => 'eng-GB',
+                )
+            ),
+            $tag
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::loadItem
+     * @expectedException \Netgen\Bundle\ContentBrowserBundle\Exceptions\NotFoundException
+     */
+    public function testLoadItemThrowsNotFoundException()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTag')
+            ->with($this->equalTo(1))
+            ->will($this->throwException(new NotFoundException('tag', 1)));
+
+        $this->backend->loadItem(1);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::getChildren
+     */
+    public function testGetChildren()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTag')
+            ->with($this->equalTo(1))
+            ->will($this->returnValue(new Tag()));
+
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTagChildren')
+            ->with(
+                $this->equalTo(new Tag()),
+                $this->equalTo(0),
+                $this->equalTo($this->config['default_limit'])
+            )
+            ->will($this->returnValue(array(new Tag(), new Tag())));
+
+        $items = $this->backend->getChildren(1);
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(Tag::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::getChildren
+     */
+    public function testGetChildrenWithEmptyItemId()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTagChildren')
+            ->with(
+                $this->equalTo(null),
+                $this->equalTo(0),
+                $this->equalTo($this->config['default_limit'])
+            )
+            ->will($this->returnValue(array(new Tag(), new Tag())));
+
+        $items = $this->backend->getChildren(0);
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(Tag::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::getChildren
+     */
+    public function testGetChildrenWithParams()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTag')
+            ->with($this->equalTo(1))
+            ->will($this->returnValue(new Tag()));
+
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTagChildren')
+            ->with(
+                $this->equalTo(new Tag()),
+                $this->equalTo(5),
+                $this->equalTo(10)
+            )
+            ->will($this->returnValue(array(new Tag(), new Tag())));
+
+        $items = $this->backend->getChildren(
+            1,
+            array(
+                'offset' => 5,
+                'limit' => 10
+            )
+        );
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(Tag::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::getChildrenCount
+     */
+    public function testGetChildrenCount()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTag')
+            ->with($this->equalTo(1))
+            ->will($this->returnValue(new Tag()));
+
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('getTagChildrenCount')
+            ->with($this->equalTo(new Tag()))
+            ->will($this->returnValue(2));
+
+        $count = $this->backend->getChildrenCount(1);
+
+        self::assertEquals(2, $count);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::getChildrenCount
+     */
+    public function testGetChildrenCountWithEmptyItemId()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('getTagChildrenCount')
+            ->with($this->equalTo(null))
+            ->will($this->returnValue(2));
+
+        $count = $this->backend->getChildrenCount(0);
+
+        self::assertEquals(2, $count);
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::search
+     */
+    public function testSearch()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTagsByKeyword')
+            ->with(
+                $this->equalTo('test'),
+                $this->equalTo('eng-GB'),
+                $this->equalTo(true),
+                $this->equalTo(0),
+                $this->equalTo($this->config['default_limit'])
+            )
+            ->will($this->returnValue(array(new Tag(), new Tag())));
+
+        $items = $this->backend->search('test');
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(Tag::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::search
+     */
+    public function testSearchWithParams()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('loadTagsByKeyword')
+            ->with(
+                $this->equalTo('test'),
+                $this->equalTo('eng-GB'),
+                $this->equalTo(true),
+                $this->equalTo(5),
+                $this->equalTo(10)
+            )
+            ->will($this->returnValue(array(new Tag(), new Tag())));
+
+        $items = $this->backend->search(
+            'test',
+            array('offset' => 5, 'limit' => 10)
+        );
+
+        self::assertCount(2, $items);
+        foreach ($items as $item) {
+            self::assertInstanceOf(Tag::class, $item);
+        }
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Backend\EzTagsBackend::searchCount
+     */
+    public function testSearchCount()
+    {
+        $this->tagsServiceMock
+            ->expects($this->once())
+            ->method('getTagsByKeywordCount')
+            ->with(
+                $this->equalTo('test'),
+                $this->equalTo('eng-GB'),
+                $this->equalTo(true)
+            )
+            ->will($this->returnValue(2));
+
+        $count = $this->backend->searchCount('test');
+
+        self::assertEquals(2, $count);
+    }
+}
