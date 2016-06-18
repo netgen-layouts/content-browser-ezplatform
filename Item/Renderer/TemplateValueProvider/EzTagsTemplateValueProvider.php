06bb11e3da04e52d21bed726525e8983fd5adfa9
Edi Modrić <edi.modric@gmail.com>
2016-06-18 16:11:34 +0200

    Remove unneeded service in template value provider

--- Item/Renderer/TemplateValueProvider/EzTagsTemplateValueProvider.php
+++ Item/Renderer/TemplateValueProvider/EzTagsTemplateValueProvider.php
@@ -4,25 +4,9 @@ namespace Netgen\Bundle\ContentBrowserBundle\Item\Renderer\TemplateValueProvider
 
 use Netgen\Bundle\ContentBrowserBundle\Item\Renderer\TemplateValueProviderInterface;
 use Netgen\Bundle\ContentBrowserBundle\Value\ValueInterface;
-use Netgen\TagsBundle\API\Repository\TagsService;
 
 class EzTagsTemplateValueProvider implements TemplateValueProviderInterface
 {
-    /**
-     * @var \Netgen\TagsBundle\API\Repository\TagsService
-     */
-    protected $tagsService;
-
-    /**
-     * Constructor.
-     *
-     * @param \Netgen\TagsBundle\API\Repository\TagsService $tagsService
-     */
-    public function __construct(TagsService $tagsService)
-    {
-        $this->tagsService = $tagsService;
-    }
-
     /**
      * Provides the values for template rendering.
      *
