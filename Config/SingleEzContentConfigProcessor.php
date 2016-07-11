9c472db316e9868aff129c4004c0960a3275d7e3
Edi Modrić <edi.modric@gmail.com>
2016-07-11 14:14:03 +0200

    Add single eZ content selection config processor

---
+++ Config/SingleEzContentConfigProcessor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Netgen\Bundle\ContentBrowserBundle\Config;
+
+use Netgen\Bundle\ContentBrowserBundle\Item\EzContent\Item;
+
+class SingleEzContentConfigProcessor implements ConfigProcessorInterface
+{
+    /**
+     * Returns the value type which this config processor supports.
+     *
+     * @return string
+     */
+    public function getValueType()
+    {
+        return Item::TYPE;
+    }
+
+    /**
+     * Returns if the processor supports the config with provided name.
+     *
+     * @param string $configName
+     *
+     * @return bool
+     */
+    public function supports($configName)
+    {
+        return $configName === 'ezcontent-single';
+    }
+
+    /**
+     * Processes the given config.
+     *
+     * @param string $configName
+     * @param \Netgen\Bundle\ContentBrowserBundle\Config\ConfigurationInterface $config
+     *
+     * @throws \Netgen\Bundle\ContentBrowserBundle\Exceptions\InvalidArgumentException If config could not be found
+     */
+    public function processConfig($configName, $config)
+    {
+        $config->setMaxSelected(1);
+    }
+}
---
+++ Tests/Config/SingleEzContentConfigProcessorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Netgen\Bundle\ContentBrowserBundle\Tests\Config;
+
+
+use Netgen\Bundle\ContentBrowserBundle\Config\Configuration;
+use Netgen\Bundle\ContentBrowserBundle\Config\SingleEzContentConfigProcessor;
+use PHPUnit\Framework\TestCase;
+
+class SingleEzContentConfigProcessorTest extends TestCase
+{
+    /**
+     * @var \Netgen\Bundle\ContentBrowserBundle\Config\SingleEzContentConfigProcessor
+     */
+    protected $configProcessor;
+
+    public function setUp()
+    {
+        $this->configProcessor = new SingleEzContentConfigProcessor();
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Config\SingleEzContentConfigProcessor::supports
+     */
+    public function testSupports()
+    {
+        self::assertTrue(
+            $this->configProcessor->supports(
+                'ezcontent-single'
+            )
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Config\SingleEzContentConfigProcessor::supports
+     */
+    public function testSupportsReturnsFalse()
+    {
+        self::assertFalse(
+            $this->configProcessor->supports(
+                'some-config'
+            )
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Config\SingleEzContentConfigProcessor::getValueType
+     */
+    public function testGetValueType()
+    {
+        self::assertEquals('ezcontent', $this->configProcessor->getValueType());
+    }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\Config\SingleEzContentConfigProcessor::processConfig
+     */
+    public function testLoadConfig()
+    {
+        $config = new Configuration('ezcontent', array('max_selected' => 0));
+        $this->configProcessor->processConfig(
+            'ezcontent-single',
+            $config
+        );
+
+        self::assertEquals(1, $config->getMaxSelected());
+    }
+}
