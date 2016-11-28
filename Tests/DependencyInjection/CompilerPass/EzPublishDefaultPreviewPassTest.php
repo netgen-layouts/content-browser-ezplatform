315923c74571057f6080d7a75c3d55deb3d052fa
Edi Modrić <edi.modric@gmail.com>
2016-11-28 22:08:50 +0100

    More tests

--- Tests/DependencyInjection/CompilerPass/EzPublishDefaultPreviewPassTest.php
+++ Tests/DependencyInjection/CompilerPass/EzPublishDefaultPreviewPassTest.php
@@ -63,4 +63,17 @@ class EzPublishDefaultPreviewPassTest extends AbstractCompilerPassTestCase
         $this->assertFalse($this->container->hasParameter('ezsettings.eng.content_view'));
         $this->assertFalse($this->container->hasParameter('ezsettings.eng.location_view'));
     }
+
+    /**
+     * @covers \Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\EzPublishDefaultPreviewPass::process
+     */
+    public function testProcessWithEmptyContainer()
+    {
+        $this->compile();
+
+        $this->assertEmpty($this->container->getAliases());
+        // The container has at least self ("service_container") as the service
+        $this->assertCount(1, $this->container->getServiceIds());
+        $this->assertEmpty($this->container->getParameterBag()->all());
+    }
 }
